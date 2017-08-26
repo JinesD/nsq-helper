c62c8c1a98a9a2692d0f139c7ede60bf722a13ed
ran.ding <ran_ding@hotmail.com>
2017-08-26 15:48:39 +0800

    [更新] 添加了消费者的处理方法

--- helper.go
+++ helper.go
@@ -9,7 +9,8 @@ import (
 
 // NSQHandler ...
 type NSQHandler struct {
-	Producer *nsq.Producer
+	Addr string
+	Cfg  *nsq.Config
 }
 
 // Publish 发布消息
@@ -18,10 +19,34 @@ func (n *NSQHandler) Publish(topic string, data interface{}) error {
 		return errors.New("nsq topic cannot empty for publish")
 	}
 
+	p, err := nsq.NewProducer(n.Addr, n.Cfg)
+	if err != nil {
+		return err
+	}
+
 	jsonData, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
 
-	return n.Producer.Publish(topic, jsonData)
+	return p.Publish(topic, jsonData)
+}
+
+// Consumer nsq 消费者
+func (n *NSQHandler) Consumer(topic string, channel string, handle func(m *nsq.Message) error) error {
+	if topic == "" || channel == "" {
+		return errors.New("topic or channel cannot empty for nsq consumer")
+	}
+
+	c, err := nsq.NewConsumer(topic, channel, n.Cfg)
+	if err != nil {
+		return err
+	}
+
+	c.AddHandler(nsq.HandlerFunc(handle))
+	if err = c.ConnectToNSQD(n.Addr); err != nil {
+		return err
+	}
+
+	return nil
 }
